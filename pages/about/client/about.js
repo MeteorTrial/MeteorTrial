9cce8d5bcae32499d194435c72a36413cc67d87a
jplayer1127 <jordan@Jordans-MacBook-Pro.local>
2016-07-12 19:49:50 -0400

    Original

---
+++ pages/about/client/about.js
@@ -0,0 +1,11 @@
+const team =
+  [
+    {name:"Tim Hickey"    , email: "tjhickey@brandeis.edu" },
+    {name:"Danielle Pike" , email: "dapike13@gmail.com" }
+  ];
+
+Template.about.helpers(
+ {
+   team
+ }
+)
---
+++ pages/userlist/server/localip.js
@@ -0,0 +1,20 @@
+
+
+Meteor.methods({
+
+  getInfo: function(){
+    const z = Meteor.user();
+    //console.dir(z);
+    var name = z.profile.name;
+    if (!name && z.emails) {
+      name = z.emails[0].address;
+    }
+    //console.log("users name is "+name);
+    Connections.remove({localIP:this.connection.clientAddress});
+    Connections.insert(
+      {userName:name,
+       localIP:this.connection.clientAddress,
+       createdAt:(new Date())});
+    return this.connection.clientAddress;
+  }
+})
